f0b06a98257addf68e6d59ae260c1cd6b96061d9
klimenko3007 <ek1830@tobii.com>
2024-06-16 16:45:25 +0200

    Text resizing based on ResizeObserver and window resize

--- src/app/components/fullscreen-text-fitter/fullscreen-text-fitter.component.ts
+++ src/app/components/fullscreen-text-fitter/fullscreen-text-fitter.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common'
-import { Component, Input } from '@angular/core'
+import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core'
 
 @Component({
   selector: 'app-fullscreen-text-fitter',
@@ -9,6 +9,43 @@ import { Component, Input } from '@angular/core'
   styleUrl: './fullscreen-text-fitter.component.scss',
 })
 export class FullscreenTextFitterComponent {
-  @Input() public text: string = ''
-  @Input() public textStyle: { [key: string]: string | number } = {} // Style input as an object
+  @Input() public text: string
+  @Input() public textStyle: { [key: string]: string | number }
+
+  @ViewChild('textElement') textElement: ElementRef<HTMLElement>
+
+  private resizeObserver: ResizeObserver
+
+  ngAfterViewInit(): void {
+    this.resizeObserver = new ResizeObserver(() => {
+      this.adjustFontSize()
+    })
+
+    this.resizeObserver.observe(this.textElement.nativeElement)
+  }
+
+  constructor(private hostElement: ElementRef) {}
+
+  @HostListener('window:resize')
+  public onResize(): void {
+    this.adjustFontSize()
+  }
+
+  ngOnDestroy(): void {
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect()
+    }
+  }
+
+  private adjustFontSize(): void {
+    let desiredWidth = this.hostElement.nativeElement.getBoundingClientRect().width
+    let currentWidth = this.textElement.nativeElement.clientWidth
+    let currentFontSize = parseFloat(
+      window.getComputedStyle(this.textElement.nativeElement).fontSize,
+    )
+
+    let newFontSize = (desiredWidth / currentWidth) * currentFontSize
+
+    this.textElement.nativeElement.style.fontSize = `${newFontSize}px`
+  }
 }
